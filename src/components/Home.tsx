2fc7cfa80d2712f4ed5b752368d413ad66afc72b
deviCarlos <carlos.rubio@devitech.com.co>
2023-03-01 18:08:42 -0500

    svg implementado

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,13 @@
 import { View, ImageBackground, Image } from "react-native";
 import { styles } from '../styles/index';
- export default function Prueba() {
+import Svg, { G, Path } from 'react-native-svg';
+export default function Home() {
   return (
     <View style={{ flex: 1 }}>
-    {/* Aquí puedes añadir tus elementos hijos */}
-  </View>
-    
+      <Svg width="250" height="250" viewBox="0 0 192.756 192.756">
+          <Path fill="#182257" d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"/>
+      </Svg>
+    </View>
+
   );
 }
